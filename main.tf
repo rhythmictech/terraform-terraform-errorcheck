98bffb6c0233c7573f26733be389393e920db143
Steven Black <genseb7@gmail.com>
2022-04-22 14:03:01 -0400

    por que no los dos

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
+
+locals {
+  jq_program = ["/bin/sh", "-efu", "${path.module}/bin/errorcheck.sh"]
+  py_program = ["python", "${path.module}/python/errorcheck.py"]
+  program    = var.use_jq ? local.jq_program : local.py_program
+}
+
 data "external" "this" {
-  program = ["/bin/sh", "-efu", "${path.module}/bin/errorcheck.sh"]
+  program = local.program
 
   query = {
     assert        = var.assert
